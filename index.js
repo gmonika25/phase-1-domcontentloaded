cfaab520fba9cd798df8efe86151745cc699bc70
Monika Gustilo <monikagustilo@gmail.com>
2022-07-16 15:37:30 -0700

    commit tree cb1434f5b163cdaee95064ad4dddaff88c3b9698

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const grabText = document.querySelector("#text");
+  grabText.innerHTML = "This is really cool!";
+});
+
+// console.log(document.getElementById("text"));
